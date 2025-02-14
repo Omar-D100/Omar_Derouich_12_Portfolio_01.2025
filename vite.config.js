2d375078189a0cc95bf2f809cc678a3dd6835a7a
OmarZoldik <omar10.drh@outlook.fr>
2025-02-14 16:22:56 +0100

    modif blank page .

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: 'Omar_Derouich_12_Portfolio_01.2025',
+  base: '/Omar_Derouich_12_Portfolio_01.2025/',
 });
